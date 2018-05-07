9e34fd7e6b590bf25710c40b6f206ff2d060fcd8
Filip Haglund <filip.haglund@elits.com>
2018-05-07 17:12:38 +0200

    More tests.

--- tests/Expect/CategoryTest.elm
+++ tests/Expect/CategoryTest.elm
@@ -3,22 +3,43 @@ module Expect.CategoryTest exposing (..)
 import Bitwise
 import Expect
 import Expect.Category exposing (..)
-import Fuzz exposing (int, intRange)
-import Fuzz.Opaque exposing (number, numberRange)
+import Fuzz exposing (float, floatRange, int, intRange, list, string)
+import Fuzz.Opaque exposing (appendable, appendable2, number, numberRange)
 import Test exposing (describe, fuzz3)
 
 
 test =
   describe "test some built-in things"
-    [ fuzz3 int int int "+" <|
-        \a b c -> { f = (+), a = a, b = b, c = c } |> Expect.all [ identityElement 0, associative, commutative ]
-    , fuzz3 (intRange -14 27) (intRange -14 27) (intRange -14 27) "*" <|
-        -- limit numbers to avoid multiplicative overflow
-        \a b c -> { f = (*), a = a, b = b, c = c } |> Expect.all [ zeroElement 0, identityElement 1, associative, commutative ]
-    , fuzz3 number number number "max" <|
-        \a b c -> { f = max, a = a, b = b, c = c } |> Expect.all [ zeroElement (1 / 0), identityElement (-1 / 0), associative, commutative, idempotent ]
-    , fuzz3 number number number "min" <|
-        \a b c -> { f = min, a = a, b = b, c = c } |> Expect.all [ zeroElement (-1 / 0), identityElement (1 / 0), associative, commutative, idempotent ]
+    [ describe "+"
+        [ fuzz3 int int int "+ int" <|
+            \a b c -> { f = (+), a = a, b = b, c = c } |> Expect.all [ identityElement 0, associative, commutative ]
+        , fuzz3 float float float "+ float" <|
+            \a b c -> { f = (+), a = a, b = b, c = c } |> Expect.all [ identityElement 0, commutative ]
+        ]
+    , describe "++"
+        [ fuzz3 appendable appendable appendable "++ appendable" <|
+            \a b c -> { f = (++), a = a, b = b, c = c } |> Expect.all [ associative ]
+        , fuzz3 appendable2 appendable2 appendable2 "++ appendable2" <|
+            \a b c -> { f = (++), a = a, b = b, c = c } |> Expect.all [ associative ]
+        , fuzz3 string string string "++ string" <|
+            \a b c -> { f = (++), a = a, b = b, c = c } |> Expect.all [ identityElement "", associative ]
+        , fuzz3 (list int) (list int) (list int) "++ list int" <|
+            \a b c -> { f = (++), a = a, b = b, c = c } |> Expect.all [ identityElement [], associative ]
+        ]
+    , describe "*"
+        [ fuzz3 (intRange -14 27) (intRange -14 27) (intRange -14 27) "* int" <|
+            -- limit numbers to avoid multiplicative overflow
+            \a b c -> { f = (*), a = a, b = b, c = c } |> Expect.all [ zeroElement 0, identityElement 1, associative, commutative ]
+        , fuzz3 (floatRange -14 27) (floatRange -14 27) (floatRange -14 27) "* float" <|
+            -- limit numbers to avoid multiplicative overflow
+            \a b c -> { f = (*), a = a, b = b, c = c } |> Expect.all [ zeroElement 0, identityElement 1, commutative ]
+        ]
+    , describe "min/max"
+        [ fuzz3 number number number "max" <|
+            \a b c -> { f = max, a = a, b = b, c = c } |> Expect.all [ zeroElement (1 / 0), identityElement (-1 / 0), associative, commutative, idempotent ]
+        , fuzz3 number number number "min" <|
+            \a b c -> { f = min, a = a, b = b, c = c } |> Expect.all [ zeroElement (-1 / 0), identityElement (1 / 0), associative, commutative, idempotent ]
+        ]
     ]
 
 
